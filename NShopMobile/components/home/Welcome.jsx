58f0423cd1433874f76e181a6f92cb0a299b047c
Soul Breaker <SoulBreaker2k3@gmail.com>
2024-10-01 22:13:22 +0700

    added search bar in home screen

--- NShopMobile/components/home/Welcome.jsx
+++ NShopMobile/components/home/Welcome.jsx
@@ -3,29 +3,31 @@ import React from 'react'
 import styles from './welcome.style'
 import { COLORS, SIZES } from '../../constants/theme'
 import { Feather, Ionicons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
 
 const Welcome = () => {
-  return (
-    <View>
-        <View style={styles.container}>
-            <Text style={styles.welcomeText(COLORS.black, SIZES.xSmall)}> Find Your </Text>
-            <Text style={styles.welcomeText(COLORS.primary, 0)}> Electronic Components</Text>
-        </View>
-        <View style={styles.searchContainer}>
-            <View style={styles.searchWrapper}>
-                <TextInput
-                    style={styles.searchInput}
-                    value=""
-                    onPressIn={() => {}}
-                    placeholder="Search Product Name" 
-                />
+    const navigation = useNavigation();
+    return (
+        <View>
+            <View style={styles.container}>
+                <Text style={styles.welcomeText(COLORS.black, SIZES.xSmall)}> Find Your </Text>
+                <Text style={styles.welcomeText(COLORS.primary, 0)}> Electronic Components</Text>
             </View>
-            <TouchableOpacity>
-                <Feather name="search" size={24} style={styles.searchIcon} />
-            </TouchableOpacity>
+            <View style={styles.searchContainer}>
+                <View style={styles.searchWrapper}>
+                    <TextInput
+                        style={styles.searchInput}
+                        value=""
+                        onPressIn={() => navigation.navigate("Search")}
+                        placeholder="Search Product Name"
+                    />
+                </View>
+                <TouchableOpacity>
+                    <Feather name="search" size={24} style={styles.searchIcon} />
+                </TouchableOpacity>
+            </View>
         </View>
-    </View>
-  )
+    )
 }
 
 export default Welcome
