68bb49de2fc37dc35a8e5da5fdff120f265b649d
Soul Breaker <SoulBreaker2k3@gmail.com>
2024-10-05 00:51:09 +0700

    product detail and rating

--- NShopMobile/components/products/ProductRow.jsx
+++ NShopMobile/components/products/ProductRow.jsx
@@ -2,22 +2,22 @@ import { FlatList, Text, View } from 'react-native'
 import React from 'react'
 import { SIZES } from '../../constants/theme';
 import ProductCartView from './ProductCartView';
+import styles from './productRow.style';
 
 const ProductRow = () => {
     const products = [1, 2, 3, 4];
 
     return (
-        <View style={{ marginTop: SIZES.small, marginHorizontal: SIZES.small }}>
+        <View style={styles.container}>
             <FlatList
                 data={products}
                 renderItem={({ item }) => (
                     <ProductCartView />
                 )}
                 horizontal
-                contentContainerStyle={{ columnGap: SIZES.medium}}
+                contentContainerStyle={{ columnGap: SIZES.xSmall}}
             />
         </View>
-
     )
 }
 
--- NShopMobile/screens/ProductDetail.jsx
+++ NShopMobile/screens/ProductDetail.jsx
@@ -1,14 +1,90 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import React, {useState} from 'react'
+import styles from './productDetail.style'
+import { Fontisto, Ionicons, MaterialCommunityIcons, SimpleLineIcons } from '@expo/vector-icons'
+import { COLORS, SIZES } from '../constants/theme'
 
-const ProductDetail = () => {
+const ProductDetail = ({ navigation }) => {
+  const [count, setCount] = useState(1);
+  const increment = () => {
+    if (count < 5)
+    setCount(count + 1)
+  }
+  const decrement = () => {
+    if (count > 1)
+      setCount(count - 1)
+  }
   return (
-    <View>
-      <Text>ProductDetail</Text>
+    <View style={styles.container}>
+      <View style={styles.upperRow}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionicons name="chevron-back-circle" size={30} />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => { }}>
+          <Ionicons name="heart" size={30} color={COLORS.primary} />
+        </TouchableOpacity>
+      </View>
+      <Image
+        source={{ uri: "https://nshopvn.com/wp-content/uploads/2019/03/xbee-shield-8M9P-s4-600x600.jpg" }}
+        style={styles.image}
+      />
+      <View style={styles.detail}>
+        <View style={styles.titleRow}>
+          <Text style={styles.title}>Product</Text>
+          <View style={styles.priceWrapper}>
+            <Text style={styles.price}>$60.88</Text>
+          </View>
+        </View>
+        <View style={styles.ratingRow}>
+          <View style={styles.rating}>
+            {[1, 2, 3, 4, 5].map((index) => (
+              <Ionicons
+                key={index}
+                name="star"
+                size={24}
+                color={index < 4 ? COLORS.primary : COLORS.secondary}
+              />
+            ))}
+            <Text style={styles.ratingText}>(3.0)</Text>
+          </View>
+          <View style={styles.rating}>
+            <TouchableOpacity onPress={() => decrement()}>
+              <SimpleLineIcons name="minus" size={20} color={COLORS.primary} />
+            </TouchableOpacity>
+            <Text style={styles.ratingText}>{count}</Text>
+            <TouchableOpacity onPress={() => increment()}>
+              <SimpleLineIcons name="plus" size={20} color={COLORS.primary} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.descriptionWrapper}>
+          <Text style={styles.description}>Description</Text>
+          <Text style={styles.descriptionText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deserunt earum saepe tenetur ducimus odio, dolorum culpa, nisi dolores nesciunt totam iusto praesentium iure debitis! Ullam saepe nam itaque veritatis?</Text>
+        </View>
+        <View style={{marginBottom: SIZES.small}}>
+          <View style={styles.location}>
+            <View style={{flexDirection: "row"}}>
+              <Ionicons name="location-outline" size={20} color={COLORS.primary} />
+              <Text>   Hanoi, Vietnam   </Text>
+            </View>
+            <View style={{flexDirection: "row"}}>
+              <MaterialCommunityIcons name="truck-delivery-outline" size={20} color={COLORS.primary} />
+              <Text>   Free Delivery   </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.cartRow}>
+          <TouchableOpacity onPress={() => {}} style={styles.cartButton}>
+            <Text style={styles.cartTitle}>Add to Cart</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}} style={styles.addCart}>
+            <Fontisto name="shopping-bag" size={20} color={COLORS.lightWhite} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   )
 }
 
 export default ProductDetail
-
-const styles = StyleSheet.create({})
